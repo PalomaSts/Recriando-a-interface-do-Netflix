11c63754f976f98f31e2e071fbeef179497b0a85
PalomaSts <paloma.sts28@gmail.com>
2021-03-03 15:37:55 -0300

    arquivos do desafio

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let mensagem = document.querySelector(".mensagem") ;
+
+// mostra a mensagem
+function showMessage(){   
+    mensagem.style.display = "block";   
+}
+
+// esconde a mensagem
+function hideMessage(){
+    mensagem.style.display = "none"; 
+}
+
+function imprimir(item){
+    return item;
+}
